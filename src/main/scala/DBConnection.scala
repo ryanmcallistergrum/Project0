558b54e30a907f580a7408aacedde9871c3a0d75
ryanmcallistergrum <>
2021-12-04 18:20:32 -0600

    Updated UML Class Diagram and .scala file to make all of DBConnection's attributes private.

--- src/main/scala/DBConnection.scala
+++ src/main/scala/DBConnection.scala
@@ -1,11 +1,25 @@
 import com.mongodb.{ServerApi, ServerApiVersion}
 import org.mongodb.scala.{ConnectionString, MongoClient, MongoClientSettings, MongoDatabase}
 
+class DBConnection {
+  private final val CONNECTION_STRING:String = "mongodb://localhost:27017";
+  private final val CLIENT_SETTINGS:MongoClientSettings = MongoClientSettings.builder().applyConnectionString(ConnectionString(CONNECTION_STRING)).serverApi(ServerApi.builder().version(ServerApiVersion.V1).build()).build();
+  private final val DB_NAME:String = "farmSim";
+  private final val CLIENT:MongoClient = MongoClient(CLIENT_SETTINGS);
+
+  protected def connect() : MongoDatabase = {
+    CLIENT.getDatabase(DB_NAME);
+  }
+  protected def disconnect() : Unit = {
+    CLIENT.close();
+  }
+}
+
 object DBConnection {
   private final val CONNECTION_STRING:String = "mongodb://localhost:27017";
   private final val CLIENT_SETTINGS:MongoClientSettings = MongoClientSettings.builder().applyConnectionString(ConnectionString(CONNECTION_STRING)).serverApi(ServerApi.builder().version(ServerApiVersion.V1).build()).build();
-  protected final val DB_NAME:String = "farmSim";
-  protected final val CLIENT:MongoClient = MongoClient(CLIENT_SETTINGS);
+  private final val DB_NAME:String = "farmSim";
+  private final val CLIENT:MongoClient = MongoClient(CLIENT_SETTINGS);
 
   protected def connect() : MongoDatabase = {
     CLIENT.getDatabase(DB_NAME);
